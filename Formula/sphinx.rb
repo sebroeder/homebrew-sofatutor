0052fa443cdbf9f2e615b156447b08de856729de
Sebastian Röder <sebroeder@users.noreply.github.com>
2019-02-16 23:42:54 +0100

    Fix error caused by deprecated check for old compilers
    Pull in the latest changes from the upstream formula to fix the following error message from Homebrew:
    
    ```
    Error: Calling 'fails_with :clang' with 'build' < 600 is disabled! There is no replacement.
    Please report this to the sebroeder/sofatutor tap:
      /usr/local/Homebrew/Library/Taps/sebroeder/homebrew-sofatutor/Formula/sphinx.rb:30
    
    If reporting this issue please do so at (not Homebrew/brew or Homebrew/core):
      https://github.com/sebroeder/homebrew-sofatutor/issues
    ```
    
    Also remove the bottles because they are not build against MySQL 5.7 and would render this custom formula useless.

--- Formula/sphinx.rb
+++ Formula/sphinx.rb
@@ -3,33 +3,11 @@ class Sphinx < Formula
   homepage "https://sphinxsearch.com/"
   url "https://sphinxsearch.com/files/sphinx-2.2.11-release.tar.gz"
   sha256 "6662039f093314f896950519fa781bc87610f926f64b3d349229002f06ac41a9"
+  revision 2
   head "https://github.com/sphinxsearch/sphinx.git"
 
-  bottle do
-    sha256 "60e5aa2a956bd9c4d1e35453b7b201e33a96565331d476423f26cd62873a3f10" => :mojave
-    sha256 "b890cf523db9777c7d125842fd6b0a53fe9a7a5a4cb816389ba6f5ee6483c78d" => :high_sierra
-    sha256 "55ce34bdedf13946fa614bde50839d93135eae720f1021e2c87807d04515ab18" => :sierra
-    sha256 "c75e018d69afb7d3cb662ebd129af67607d47f7b7f71ce8ea95be75d66dc502d" => :el_capitan
-    sha256 "f89b43df8735d295a55c74f18d6af4a1a10b9f3ae81df69713c27f9240f78d14" => :yosemite
-    sha256 "4ec1f1ea71e17b9e924e9f36747d7184114463640f100022cdbb46202e46261f" => :mavericks
-  end
-
-  option "with-mysql", "Force compiling against MySQL"
-  option "with-mysql@5.7", "Force compiling against MySQL 5.7"
-  option "with-postgresql", "Force compiling against PostgreSQL"
-
-  deprecated_option "mysql" => "with-mysql"
-  deprecated_option "pgsql" => "with-postgresql"
-
-  depends_on "mysql" => :optional
-  depends_on "mysql@5.7" => :optional
-  depends_on "openssl" if build.with?("mysql") || build.with?("mysql@5.7")
-  depends_on "postgresql" => :optional
-
-  fails_with :clang do
-    build 421
-    cause "sphinxexpr.cpp:1802:11: error: use of undeclared identifier 'ExprEval'"
-  end
+  depends_on "mysql@5.7"
+  depends_on "openssl"
 
   resource "stemmer" do
     url "https://github.com/snowballstem/snowball.git",
@@ -47,45 +25,12 @@ class Sphinx < Formula
       --disable-dependency-tracking
       --localstatedir=#{var}
       --with-libstemmer
+      --with-mysql
+      --without-pgsql
     ]
-
-    if build.with?("mysql") || build.with?("mysql@5.7")
-      args << "--with-mysql"
-    else
-      args << "--without-mysql"
-    end
-
-    if build.with? "postgresql"
-      args << "--with-pgsql"
-    else
-      args << "--without-pgsql"
-    end
 
     system "./configure", *args
     system "make", "install"
-  end
-
-  def caveats; <<~EOS
-    This is not sphinx - the Python Documentation Generator.
-    To install sphinx-python use pip.
-
-    Sphinx has been compiled with libstemmer support.
-
-    Sphinx depends on either MySQL or PostreSQL as a datasource.
-
-    You can install these with Homebrew with:
-      brew install mysql
-        For MySQL server.
-
-      brew install mysql-connector-c
-        For MySQL client libraries only.
-
-      brew install postgresql
-        For PostgreSQL server.
-
-    We don't install these for you when you install this formula, as
-    we don't know which datasource you intend to use.
-  EOS
   end
 
   test do
